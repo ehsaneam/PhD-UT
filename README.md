4e57d7745af96d809cf8225efc9107cb96439ae2
bijanbina <bijanbina@gmail.com>
2024-06-14 15:17:10 +0330

    Implement Network Generator

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # PhD-UT
+
+python package install: 
+```
+pip install --trusted-host https://mirror-pypi.runflare.com -i https://mirror-pypi.runflare.com/simple/ [package]
+```
--- constants.py
+++ constants.py
@@ -12,11 +12,19 @@ class Const:
     SFC_DELAY = [500, 2000]
     ALPHA_RANGE = [0.8, 1.05]
     LAMBDA_RANGE = [1, 5]
-    #
-    WIRE_LINK_PR = 0.2
-    LINK_BW = [100, 1000]
     # LINK_BW = [10000, 20000]
-    SERVER_CPU = [16, 32]
-    SERVER_RAM = [4, 32]
-    SERVER_DISK = [20000, 30000]
 
+    # Entity Numbers
+    BTS_NUM  = 10
+    EDGE_NUM = 7
+    REG_NUM  = 1
+
+    # Edge Server Values
+    EDGE_CPU = 240   # GHz
+    EDGE_RAM = 256   # GB
+    EDGE_DSK = 10000 # GB
+
+    # Regional Server Values
+    REG_CPU = 4000   # GHz
+    REG_RAM = 4000   # GB
+    REG_DSK = 160000 # GB
